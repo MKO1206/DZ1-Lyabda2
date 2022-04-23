88a15372ef0b32b1e1b637cafb89b790d1188cc3
MKO1206 <bibliotekacheb@yandex.ru>
2022-04-23 23:22:28 +0700

    Not divide by zero

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -7,9 +7,16 @@ import java.util.function.*;
 
      BinaryOperator<Integer> plus = Integer::sum;
      BinaryOperator<Integer> minus = (x, y) -> x - y;
-     BinaryOperator<Integer> divide = (x, y) -> x / y;
+     BinaryOperator<Integer> multiply = (x, y) -> x * y;
+     BinaryOperator<Integer> divide = (x, y) -> {
+         if (y != 0) return x / y;
+         else return 0;
+     };
 
+     UnaryOperator<Integer> pow = x -> x * x;
      UnaryOperator<Integer> abs = x -> x >= 0 ? x : x * -1;
+
+      Predicate<Integer> isPositive = x -> x > 0;
 
      Consumer<Integer> println = System.out::println;
   }
--- src/calc/Main.java
+++ src/calc/Main.java
@@ -9,12 +9,5 @@ public class Main {
         int c = calc.divide.apply(a, b);
 
         calc.println.accept(c);
-
-        calc.println.accept(calc.abs.apply(0));
-        calc.println.accept(calc.abs.apply(-1));
-        calc.println.accept(calc.abs.apply(1));
-
-        calc.println.accept(calc.abs.apply(Integer.MIN_VALUE));
-        calc.println.accept(calc.abs.apply(Integer.MAX_VALUE));
     }
 }
